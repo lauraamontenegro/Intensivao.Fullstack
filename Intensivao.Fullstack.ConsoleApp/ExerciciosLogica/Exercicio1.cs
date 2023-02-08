83cf6681d7bb6d5c6ebc25328f6c2500ecb16a89
Laura Montenegro <larislakis@hotmail.com>
2023-02-07 21:52:51 -0300

    Referente ao exercício 1.

---
+++ Intensivao.Fullstack.ConsoleApp/ExerciciosLogica/Exercicio1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intensivao.Fullstack.ConsoleApp.ExerciciosLogica
+{
+    public class Exercicio1
+    {
+        public static void Calcular()
+        {
+            Console.WriteLine("1º Crie um vetor com nomes de todos os integrantes do intensivão exiba todos os nomes, \r\n  e também faça com que acrescente os sobrenomes para cada integrante e exiba novamente com os nomes completos.");
+            Console.WriteLine();
+        }
+    }
+}
--- Intensivao.Fullstack.ConsoleApp/Program.cs
+++ Intensivao.Fullstack.ConsoleApp/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Intensivao.Fullstack.ConsoleApp.ExerciciosLogica;
+
+Exercicio1.Calcular();
