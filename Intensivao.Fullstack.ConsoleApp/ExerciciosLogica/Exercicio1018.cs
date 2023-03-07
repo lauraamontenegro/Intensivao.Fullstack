a0e0a48337a56b90771e9da7e8a5186f1bba6b86
Laura Montenegro <larislakis@hotmail.com>
2023-03-07 17:42:57 -0300

    exercício caixa eletrônico

---
+++ Intensivao.Fullstack.ConsoleApp/ExerciciosLogica/Exercicio1018.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Intensivao.Fullstack.ConsoleApp.ExerciciosLogica
+{
+    internal class Exercicio1018
+    {
+        public static void Cedulas()
+
+        {
+            Console.WriteLine("Digite aqui o valor que deseja retirar no caixa");
+
+            int[] notas = new int[7] { 100, 50, 20, 10, 5, 2, 1 };
+            int valor = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine(valor);
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.WriteLine(valor / notas[i] + "nota(s) de R$ " + notas[i] + ",00");
+                valor = (valor%notas[i]);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- Intensivao.Fullstack.ConsoleApp/Program.cs
+++ Intensivao.Fullstack.ConsoleApp/Program.cs
@@ -1,8 +1,9 @@
 ﻿using Intensivao.Fullstack.ConsoleApp.ExerciciosLogica;
 
-Exercicio1.Calcular();
-Exercicio2.Salario();
-Exercicio3.Media();
-Exercicio6.Peso();
-Exercicio7.Dias();
-Exercicio8.Vendas();
+//Exercicio1.Calcular();
+//Exercicio2.Salario();
+//Exercicio3.Media();
+//Exercicio6.Peso();
+//Exercicio7.Dias();
+//Exercicio8.Vendas();
+Exercicio1018.Cedulas();
